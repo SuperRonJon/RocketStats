08ba0372816a1aaa1c58fa75e26d62f617f29e00
pseute <patrick.seute@gmail.com>
2018-04-30 19:28:28 -0400

    add fetching for multiple pages

--- retrieve_data.py
+++ retrieve_data.py
@@ -1,7 +1,7 @@
 import requests
 from match import Match
 
-url = 'https://api.octane.gg/api/matches/?sort=&page=1'
+base_url = 'https://api.octane.gg/api/matches/?sort=&page='
 
 
 def create_match(match_data):
@@ -15,6 +15,31 @@ def print_to_csv(matches, filename):
     f.write(headers)
     for match in matches:
         f.write(match.csv_output())
+
+
+def get_matches_from_page(page_number):
+    url = base_url + str(page_number)
+    response = requests.get(url)
+    data = response.json()
+    matches_data = data['data']
+    if len(matches_data) == 0:
+        return None
+    else:
+        page_matches = []
+        for match in matches_data:
+            page_matches.append(create_match(match))
+        return page_matches
+
+
+def get_all_matches():
+    page = 1
+    all_matches = []
+    curr_matches = get_matches_from_page(page)
+    while curr_matches is not None:
+        all_matches.extend(curr_matches)
+        page += 1
+        curr_matches = get_matches_from_page(page)
+    return all_matches
 
 
 response = requests.get(url)
